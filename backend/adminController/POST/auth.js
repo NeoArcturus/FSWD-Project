61152987164c56b1ab69a75cdfa3e119d6c4a3f3
NeoArcturus <142579399+NeoArcturus@users.noreply.github.com>
2023-09-17 21:38:05 +0530

    Minor change

--- backend/adminController/POST/auth.js
+++ backend/adminController/POST/auth.js
@@ -3,7 +3,7 @@ const adminAuth = require("../../middleware/admin/auth");
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
+router.post("/newAdmin", async (req, res) => {
   if (req.body.id === undefined || req.body.id === null || req.body.id === "") {
     res.status(401);
     res.send({ message: "Invalid data!" });
