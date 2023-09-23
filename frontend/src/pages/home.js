81de2aecdbcb75d72f415e8f6581cda4564efaba
NeoArcturus <142579399+NeoArcturus@users.noreply.github.com>
2023-09-23 12:55:17 +0100

    Created file for home page

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+class home extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default withRouter(home);
