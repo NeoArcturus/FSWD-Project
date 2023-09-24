80e32c0d4e1af8382b8cf1a689c2de1556145234
NeoArcturus <142579399+NeoArcturus@users.noreply.github.com>
2023-09-24 18:17:57 +0100

    Initial home page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import home from "./pages/home";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home}></Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
-
-export default App;
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,14 +1,66 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
+import "../style/home.css";
+import { Button } from "react-bootstrap";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 class home extends React.Component {
   constructor(props) {
     super();
     this.state = {};
   }
 
+  loginClick = () => {
+    toast.success("Successful login!");
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          theme="dark"
+        />
+        <div id="topbarButtons">
+          <Button
+            style={{
+              backgroundColor: "black",
+              color: "deeppink",
+              fontSize: "30px",
+              borderStyle: "solid",
+              borderColor: "deeppink",
+              borderRadius: "5px",
+              marginLeft: "85vw",
+              marginTop: "5px",
+              transition: "0.4s ease-in-out",
+            }}
+          >
+            Sign Up
+          </Button>
+          <Button
+            style={{
+              backgroundColor: "black",
+              color: "deeppink",
+              fontSize: "30px",
+              borderStyle: "solid",
+              borderColor: "deeppink",
+              borderRadius: "5px",
+              marginLeft: "20px",
+              marginTop: "5px",
+              transition: "0.4s ease-in-out",
+            }}
+            onClick={() => this.loginClick()}
+          >
+            Sign In
+          </Button>
+        </div>
+      </div>
+    );
   }
 }
 
